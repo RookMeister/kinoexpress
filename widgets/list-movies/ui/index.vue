8381850f905a7830006f1642c653a494be83f0eb
RookMeister <rookmeister666@gmail.com>
2023-11-22 21:04:52 +0300

    move list-movies to widgets

--- widgets/list-movies/ui/index.vue
+++ widgets/list-movies/ui/index.vue
@@ -1,24 +1,30 @@
 <script setup lang="ts">
-import type { IFilm } from '~/shared/types/film'
 import { CardMovie } from '~/shared/ui/components/card-movie'
 import { Carousel } from '~/shared/ui/components/carousel'
 
-defineProps<{ items: IFilm[] }>()
+const props = defineProps<{ title: string; type: string }>()
+
+const { data, pending } = await useLazyFetch('/api/films/collections', {
+  method: 'GET', server: false, query: { type: props.type },
+})
 </script>
 
 <template>
   <div flex py3 px10 items-center mt5>
     <div text-2xl>
-      Популярные фильмы
+      {{ title }}
     </div>
     <div flex-auto />
     <NuxtLink to="/" n-link>
       Показать ещё
     </NuxtLink>
   </div>
-  <Carousel>
+  <div v-if="pending">
+    Loading ...
+  </div>
+  <Carousel v-if="data && !pending">
     <CardMovie
-      v-for="i of items || []"
+      v-for="i of data"
       :key="i.kinopoiskId"
       :item="i"
       flex-1 w-40 md:w-60
