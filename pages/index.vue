601ba2758bf54167dd6f3c7406d5ebb158f1dc6f
RookMeister <rookmeister666@gmail.com>
2023-11-22 16:58:31 +0300

    add index page

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ListMovies } from '~/shared/ui/components/list-movies'
+
+const { data } = await useFetch(() => '/api/films/collections', { method: 'GET' })
+</script>
+
+<template>
+  <ListMovies v-if="data" :items="data" />
+</template>
