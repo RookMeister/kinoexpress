6416a921ee72f90d09c176805085edea238aaff8
RookMeister <rookmeister666@gmail.com>
2023-11-22 16:58:04 +0300

    add api collections

---
+++ server/api/films/collections.ts
@@ -0,0 +1,25 @@
+import { KINOPOISK_API, KINOPOISK_URL } from '~/shared/const/kinopoisk'
+import type { IFilm } from '~/shared/types/film'
+
+interface IResponse {
+  items: IFilm[]
+  total: number
+  totalPages: number
+}
+
+const headers = {
+  'X-API-KEY': KINOPOISK_API,
+  'Content-Type': 'application/json',
+}
+
+export default defineEventHandler(async () => {
+  try {
+    const url = `${KINOPOISK_URL}/api/v2.2/films/collections?type=TOP_POPULAR_MOVIES&page=1`
+    const { items } = await $fetch<IResponse>(url, { method: 'GET', headers })
+
+    return items
+  }
+  catch (error) {
+    console.log(error)
+  }
+})
